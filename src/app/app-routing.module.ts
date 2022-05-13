6e71fd625c79def5dbab8a3887bd365ba4a799f4
markup54 <marzenakupka@tlen.pl>
2022-05-13 15:21:42 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { FirstComponent } from './first/first.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'pierwszy' ,
+  component: FirstComponent,
+  children:[
+    {path:'dzieckiA',component:ChildAComponent},
+    {path:'dzieckiB',component:ChildBComponent}
+  ]
+},
+  {path:'drugi', component: SecondComponent},
+  {path:'',redirectTo:'/pierwszy',pathMatch:'full'},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
